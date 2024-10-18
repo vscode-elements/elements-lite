1ca319ae09e554f6a4af1778a4a01e34f1d82798
bendera <szoftverhiba@gmail.com>
2024-10-19 00:27:03 +0200

    Add react integration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,40 +2,40 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "VSCode Elements Lite",
-      logo: {
-        src: "/src/assets/logo.svg",
+  integrations: [starlight({
+    title: "VSCode Elements Lite",
+    logo: {
+      src: "/src/assets/logo.svg",
+    },
+    social: {
+      github: "https://github.com/vscode-elements/elements-lite",
+    },
+    sidebar: [
+      {
+        label: "Components",
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: "Action Button", slug: "components/action-button" },
+          { label: "Badge", slug: "components/badge" },
+          { label: "Button", slug: "components/button" },
+        ],
       },
-      social: {
-        github: "https://github.com/vscode-elements/elements-lite",
+      {
+        label: "Guides",
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: "Example Guide", slug: "guides/example" },
+        ],
       },
-      sidebar: [
-        {
-          label: "Components",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Action Button", slug: "components/action-button" },
-            { label: "Badge", slug: "components/badge" },
-            { label: "Button", slug: "components/button" },
-          ],
-        },
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-      customCss: ["./src/styles/custom.css"],
-    }),
-  ],
+      {
+        label: "Reference",
+        autogenerate: { directory: "reference" },
+      },
+    ],
+    customCss: ["./src/styles/custom.css"],
+  }), react()],
 });
